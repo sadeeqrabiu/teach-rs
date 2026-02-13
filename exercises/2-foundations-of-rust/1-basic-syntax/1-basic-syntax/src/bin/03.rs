b61e9e657a01cf56843385546030ce2091977073
Abubakar Sadiq <53898703+sadeeqrabiu@users.noreply.github.com>
2026-02-13 05:21:15 +0100

    1-syntax solved

--- exercises/2-foundations-of-rust/1-basic-syntax/1-basic-syntax/src/bin/03.rs
+++ exercises/2-foundations-of-rust/1-basic-syntax/1-basic-syntax/src/bin/03.rs
@@ -2,6 +2,22 @@ fn main() {
     let input = [23, 82, 16, 45, 21, 94, 12, 34];
 
     // TODO
+    let largest = input.iter().max().unwrap();
+    let smallest = input.iter().min().unwrap();
 
-    println!("{} is largest and {} is smallest");
+    println!("{} is largest and {} is smallest", largest, smallest);
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn emppty_array_should_return_none() {
+        let input = [];
+        let largest = input.iter().max().unwrap();
+        let smallest = input.iter().min().unwrap();
+        assert_eq!(largest, None);
+        assert_eq!(smallest, None);
+    }
 }
--- exercises/2-foundations-of-rust/1-basic-syntax/1-basic-syntax/src/bin/05.rs
+++ exercises/2-foundations-of-rust/1-basic-syntax/1-basic-syntax/src/bin/05.rs
@@ -1,10 +1,12 @@
 fn main() {
     let data = [22, 12, 13, 17, 18];
+    let mut result = [0; 5];
     for i in 0..5 {
-        data.i = floored_half(data.i);
+        result[i] = floored_half(data[i]);
     }
+    println!("{:?}", result);
 }
 
-fn floored_half(data: i32) {
+fn floored_half(data: i32) -> i32 {
     data / 2
 }
